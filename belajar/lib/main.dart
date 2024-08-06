fc228700b525403bceb0aa86df234dce4dd78971
superlufi <syaifulbahrialhaq@yandex.com>
2024-08-06 16:59:01 +0700

    Test MaterialApp Theme

--- belajar/lib/main.dart
+++ belajar/lib/main.dart
@@ -16,9 +16,11 @@ class MyApp extends StatelessWidget {
       //color: const Color.fromARGB(255, 255, 255, 255),
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorSchemeSeed: const Color.fromARGB(255, 255, 255, 255),
-        useMaterial3: true,
-        brightness: Brightness.light,
+        hoverColor: Colors.blue,
+        colorSchemeSeed: Color.fromARGB(255, 255, 255, 255),
+        useMaterial3: false,
+        brightness: Brightness.values[0],
+        //primaryColor: Color.fromRGBO(255, 255, 255, 0),
       ),
       home: const MyHomePage(
         title: 'Flutter Demo Home Page',
@@ -95,3 +97,103 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+
+// Test Widget text
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(
+//     const Center(
+//       child: Text(
+//         'Hello Flutter, world!',
+//         textDirection: TextDirection.rtl,
+//         selectionColor: Colors.red,
+//         style: TextStyle(
+//           fontSize: 30,
+//           color: Colors.blue,
+//         ),
+//       ),
+//     ),
+//   );  
+// }
+
+// import 'package:flutter/material.dart';
+
+// class MyAppBar extends StatelessWidget {
+//   const MyAppBar({required this.title, super.key});
+
+//   // Fields in a Widget subclass are always marked "final".
+
+//   final Widget title;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       height: 56, // in logical pixels
+//       padding: const EdgeInsets.symmetric(horizontal: 8),
+//       decoration: BoxDecoration(color: Colors.blue[500]),
+//       // Row is a horizontal, linear layout.
+//       child: Row(
+//         children: [
+//           const IconButton(
+//             icon: Icon(Icons.menu),
+//             tooltip: 'Navigation menu',
+//             onPressed: null, // null disables the button
+//           ),
+//           // Expanded expands its child
+//           // to fill the available space.
+//           Expanded(
+//             child: title,
+//           ),
+//           const IconButton(
+//             icon: Icon(Icons.search),
+//             tooltip: 'Search',
+//             onPressed: null,
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// class MyScaffold extends StatelessWidget {
+//   const MyScaffold({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // Material is a conceptual piece
+//     // of paper on which the UI appears.
+//     return Material(
+//       // Column is a vertical, linear layout.
+//       child: Column(
+//         children: [
+//           MyAppBar(
+//             title: Text(
+//               'Example title',
+//               style: Theme.of(context) //
+//                   .primaryTextTheme
+//                   .titleLarge,
+//             ),
+//           ),
+//           const Expanded(
+//             child: Center(
+//               child: Text('Hello, world!'),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// void main() {
+//   runApp(
+//     const MaterialApp(
+//       title: 'My app', // used by the OS task switcher
+//       home: SafeArea(
+//         child: MyScaffold(),
+//       ),
+//     ),
+//   );
+// }
